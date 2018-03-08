9d33654aff0c3d57f95ac47d97f45b6215eb79b6
liuyingjie <1193418905@qq.com>
2018-03-08 10:44:19 +0800

    eureka配置

--- eurekaserver/src/main/java/com/jm/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/jm/eurekaserver/EurekaserverApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EnableEurekaServer //启动eureka服务
 @SpringBootApplication
 public class EurekaserverApplication {
 
