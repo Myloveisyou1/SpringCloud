c4590e7858980c514b452f672b2fe4109ded5519
liuyingjie <1193418905@qq.com>
2018-03-08 10:58:51 +0800

    系统初始化

---
+++ wagesystem/src/main/java/com/jm/wagesystem/WagesystemApplication.java
@@ -0,0 +1,15 @@
+package com.jm.wagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaClient //表明自己是客户端client
+@SpringBootApplication
+public class WagesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagesystemApplication.class, args);
+	}
+}
