59e690b1324dfcba30c54ed648f2945bcec40c59
liuyingjie <1193418905@qq.com>
2018-03-10 10:52:31 +0800

    实现springcloud的断路器hystrix功能

--- wagesystem/src/main/java/com/jm/wagesystem/WagesystemApplication.java
+++ wagesystem/src/main/java/com/jm/wagesystem/WagesystemApplication.java
@@ -3,8 +3,10 @@ package com.jm.wagesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient //表明自己是客户端client
+@EnableFeignClients //开启Feign的功能
 @SpringBootApplication
 public class WagesystemApplication {
 
