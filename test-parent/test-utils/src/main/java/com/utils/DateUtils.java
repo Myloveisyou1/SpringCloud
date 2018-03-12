0141414cffd7932472425fb281b40f20dbcbeb3d
liuyingjie <1193418905@qq.com>
2018-03-12 10:58:28 +0800

    日期工具类  新增 1.获取当前周一到周天的日期 2.格式化日期到指定的格式 3.获取某个日期的最后一天

--- test-parent/test-utils/src/main/java/com/utils/DateUtils.java
+++ test-parent/test-utils/src/main/java/com/utils/DateUtils.java
@@ -16,6 +16,20 @@ public class DateUtils {
      * @see 测试时，将其放到<code>calendar.add(Calendar.DATE, -1);</code>前面即可
      * @return 返回的日期格式为yyyy-MM-dd
      */
+	/**
+	 * 格式化日期
+	 * @param type   格式化类型
+	 * @param date   要格式化的日期
+	 * @return
+	 */
+	public static String getSimpleDateFormatDays(String type, Date date) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat(type);
+
+		return sdf.format(date);
+	}
+
+
     public static String getYestoday(){
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, -1);
@@ -49,24 +63,64 @@ public class DateUtils {
         String lastday = sdf.format(date);  
         
 		return lastday;
-		
 	}
- // 获得本周星期一的日期  
-    public static String getCurrentMonday() {  
+
+	/**
+	 * 获取某个时间月份下的最后一天
+	 * @param year   年份
+	 * @param month  月份
+	 * @return
+	 */
+	public static String getLastDayOfMonth(int year,int month) {
+		if(month < 1 || month > 12) {
+			return "参数错误,请检查!";
+		}
+		Calendar cal = Calendar.getInstance();
+		//设置年份
+		cal.set(Calendar.YEAR,year);
+		//设置月份
+		cal.set(Calendar.MONTH, month-1);
+		//获取某月最大天数
+		int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		//设置日历中月份的最大天数
+		cal.set(Calendar.DAY_OF_MONTH, lastDay);
+		//格式化日期
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String lastDayOfMonth = sdf.format(cal.getTime());
+
+		return lastDayOfMonth;
+	}
+
+	/**
+	 * 获取当前周的周一到周日
+ 	 * @param day  获取的周几
+	 * @return
+	 */
+    public static String getCurrentMondayToSunday(int day) {
+
+    	if(day < 1 || day > 7) {
+    		return "参数错误,请选择周一到周日";
+		}
     	Calendar c = Calendar.getInstance();
     	  int day_of_week = c.get(Calendar.DAY_OF_WEEK) - 1;
     	  if (day_of_week == 0){
     		  day_of_week = 7;
     	  }
-    	  c.add(Calendar.DATE, -day_of_week + 1);
+    	  c.add(Calendar.DATE, -day_of_week + day);
     	  return new SimpleDateFormat("yyyy-MM-dd").format(c.getTime()); 
     } 
     
     public static void main(String[] args) throws ParseException {
 		
-    	System.out.println(getCurrentMonday());
-    	
-    	Double money = 0.00;
+    	System.out.println(getSimpleDateFormatDays("yyyyMMddHHmmss",new Date()));
+    	System.out.println(getYestoday());
+    	System.out.println(getToday());
+    	System.out.println(getTomorrow());
+    	System.out.println(lastDayOfMonth("2017-12-12"));
+    	System.out.println(getLastDayOfMonth(2017,2));
+    	System.out.println(getCurrentMondayToSunday(7));
+
+    	/*Double money = 0.00;
     	double m = 0.01;
     	for(int i=1;i<=30;i++){
     		if(i != 1){
@@ -77,6 +131,6 @@ public class DateUtils {
     		}
     		//System.out.println(i+"  "+m+"   "+money);
     	}
-    	System.out.println("总金额："+money);
+    	System.out.println("总金额："+money);*/
 	}
 }
